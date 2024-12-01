fe0c4f8f60a2c5ad5783cf4e067a5a2521df4b02
jefrir531 <jefrir531@gmail.com>
2024-12-01 13:28:20 -0500

    Se agrega el proyecto

---
+++ src/cliente/CLIENTE.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cliente;
+
+/**
+ *
+ * @author User
+ */
+public class CLIENTE {
+    
+    public static void main(String[]args){
+        
+        VENTANA_CLIENTE_ costumer = new VENTANA_CLIENTE_  ();
+        costumer.setVisible(true); //para mostrar el frame
+    }
+}
---
+++ src/servidor/SERVIDOR.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package servidor;
+
+/**
+ *
+ * @author User
+ */
+public class SERVIDOR {
+    
+    public static void main (String [] args){
+        
+        VENTANA_SERVIDOR_ service = new VENTANA_SERVIDOR_();
+        service.setVisible(true);
+    }
+}
